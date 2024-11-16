3cbbeb32ebd58c3836ae6b008cc606be36a7a845
Nikita Yeldynov <yeldynov@gmail.com>
2024-11-16 15:34:07 +0200

    basic setup with top navbar

---
+++ src/components/Providers.tsx
@@ -0,0 +1,6 @@
+import React, { ReactNode } from 'react'
+import { NextUIProvider } from '@nextui-org/react'
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>
+}
